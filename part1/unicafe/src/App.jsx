05fd533b61e2f5b0c6e651a5031e4fa8ea86e19b
Miguel Carcel <rustimen@hotmail.com>
2025-05-15 10:44:26 +0200

    1.6: unicafe, step 1 Basic configuration

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,31 +2,23 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-
-  const [allClicks, setAll] = useState([])
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
+  const [positives, setLeft] = useState(0)
+  const [neutrals, setNeutral] = useState(0)
+  const [negatives, setRight] = useState(0)
+  
+  
   return (
     <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-
-      <p>{allClicks.join(' ')}</p>
+      <h1>Give Feedback</h1>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      
+      <h1>Statistics</h1>
+      <p>Good: {positives}</p>
+      <p>Neutral: {neutrals}</p>
+      <p>Bad: {negatives}</p>
+      <p>All: {positives + neutrals + negatives}</p>
     </div>
   )
 }
