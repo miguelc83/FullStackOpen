d8dbff6fe57a81baa58c9011bbdb40270dc36363
Miguel Carcel <rustimen@hotmail.com>
2025-05-17 08:06:38 +0200

    2.8: La Agenda Telefónica Paso 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,24 +2,40 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      phone: '040-123456'
+     }
   ]) 
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+ 
 
 const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      phone: newNumber
     }
     if (!NameValidator(newName, persons)) {
       return
+    }else if (newNumber === '') {
+      alert('Please enter a number')
+      return
     }
+    // Check if the name already exists in the persons array
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   // NameValidator function to check if the name is empty or already exists
@@ -43,6 +59,8 @@ const addName = (event) => {
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+          <br />
+          phone: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit" onClick={addName}>add</button>
@@ -51,7 +69,7 @@ const addName = (event) => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>{person.name} {person.phone}</li>
         ))}
       </ul>
     </div>
